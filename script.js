2c36e042df762cbf35ba8e84afceb7d73087b909
llenk <ellenkeal@gmail.com>
2018-04-09 14:25:50 -0500

    afternoon lecture

--- script.js
+++ script.js
@@ -56,31 +56,31 @@ if (0) { //this doesn't go
     console.log('nayyyy');
 }
 if (-1) { //this goes
-    console.log('ayyyy');
+    console.log('ayyyy'); //true
 }
 if (firstName) {
-    console.log('#yolo');
+    console.log('#yolo'); //true
 }
 if ('') {
-    console.log('#nolo');
+    console.log('#nolo'); //false
 }
 if (!'') {
-    console.log('not empty string??');
+    console.log('not empty string??'); //true
 }
 if ({}) {
-    console.log('empty Object is true');
+    console.log('empty Object is true'); //true
 }
 if ([]) {
-    console.log('empty array');
+    console.log('empty array'); //true
 }
 if (undefined) {
-    console.log('undefined');
+    console.log('undefined'); //false
 }
 if (null) {
-    console.log('null');
+    console.log('null'); //false
 }
 if (typeof 7 == 'number') {
-    console.log('typeof7');
+    console.log('typeof7'); //true
 }
 if (typeof NaN == 'number') {
     console.log('NaN has data type number wuuuut');
@@ -157,3 +157,48 @@ for (let i=1; i<hats.length; i++) { //skip first one
 for (let i=1; i<hats.length; i+=2) { //do every other one
     console.log('a ' + hats[i] + ' is a type of hat');
 }
+
+//Functions
+
+//function declaration
+//this kind of function is HOISTED which means that when JS goes through it the first time 
+//this function is checked out and then the code is run the second time so no matter where you write 
+//it (top or bottom) it'll work
+function doubleIt(n) {
+    return 2*n;
+} 
+
+const doubledNumber = doubleIt(4);
+console.log(doubledNumber);
+
+//function expression
+//this kind HAS TO be called after being expressed bc if it was a let it could be changed 
+//so it has to be placed right. Not hoisted. You'll get an uncaught reference error if you mess it up
+const tripleIt = function(numberToTriple) {
+    return numberToTriple*3;
+}
+
+console.log(tripleIt(7));
+console.log(tripleIt(60));
+
+//Built-In Functions
+console.log(Number('8')); //number data type
+console.log(String(8)); //string data type
+let eight = 8;
+console.log(eight.toString()); //string data type
+console.log(eight); //number data type
+
+let numberArray = [1,4,7,4,3,5,3];
+
+console.log(numberArray.pop()); //returns the thing it popped off
+numberArray.shift();
+console.log(numberArray.unshift(0)); //adds to front, returns the length
+
+//stuff can alter and return whatever it wants #yolo
+
+//this is a method, which we can tell bc it's got the dot
+//there's a difference between function and methods
+numberArray.push(42);
+
+console.log(numberArray);
+
